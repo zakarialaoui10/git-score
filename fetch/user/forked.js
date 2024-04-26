3b9d749c4b317626e05f96be3f334d3e57a5ab8b
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2024-04-26 17:12:26 +0100

    Update forked.js

--- fetch/user/forked.js
+++ fetch/user/forked.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// Function to get forked repositories of a GitHub user
-async function getForkedRepositories(username) {
+async function getUserRepositories(username, includeForks = true,source = true, sortBy = null) {
     try {
         let repositories = [];
         let page = 1;
@@ -9,52 +8,54 @@ async function getForkedRepositories(username) {
 
         while (hasNextPage) {
             const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
+              headers: {
+                  Authorization: `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`
+              },
                 params: {
-                    per_page: 100, // Maximum number of items per page
+                    per_page: 100, 
                     page: page
                 }
+              
             });
-
-            // Filter repositories to find the forked ones
-            const forkedRepos = response.data.filter(repo => repo.fork);
-
-            // Add forked repositories from the current page to the array
-            for (const repo of forkedRepos) {
-                const parentResponse = await axios.get(repo.url); // Get full details of forked repo
-
-                repositories.push({
-                    name: repo.name,
-                    owner: parentResponse.data.owner.login,
-                    full_name: parentResponse.data.full_name
-                });
-            }
-
-            // Check if there is another page
+          let reposToAdd = response.data;
+          if(!includeForks) reposToAdd=reposToAdd.filter(repo => !repo.fork);
+          if(!source) reposToAdd=reposToAdd.filter(repo => repo.fork)
+            repositories = repositories.concat(reposToAdd);
             if (response.headers.link) {
                 const linkHeader = response.headers.link;
                 hasNextPage = linkHeader.includes('rel="next"');
             } else {
                 hasNextPage = false;
             }
-
-            // Move to the next page
             page++;
         }
-
-        return repositories;
+        if (sortBy) {
+            repositories.sort((a, b) => {
+                if (sortBy === 'stars') {
+                    return b.stargazers_count - a.stargazers_count;
+                } else if (sortBy === 'last_updated') {
+                    return new Date(b.updated_at) - new Date(a.updated_at);
+                } else {
+                    return 0;
+                }
+            });
+        }
+        return repositories.map(repo => repo.name);
     } catch (error) {
         throw error;
     }
 }
 
-// Replace 'username' with the GitHub username you want to get forked repositories for
+// Test
 const username = 'zakarialaoui10';
+const includeForks = true; 
+const includeSources = false;
+const sortBy = 'stars'; 
 
-getForkedRepositories(username)
+getUserRepositories(username, includeForks,includeSources, sortBy)
     .then(repositories => {
         console.log(repositories);
     })
     .catch(error => {
         console.error('Error:', error.message);
     });
-
