181f418bc6dc11532bd884e157578726025a0359
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2024-04-26 11:53:40 +0100

    Update all-repo.js

--- fetch/all-repo.js
+++ fetch/all-repo.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function getUserRepositories(username) {
+async function getUserRepositories(username, includeForks = true, sortBy = null) {
     try {
         let repositories = [];
         let page = 1;
@@ -9,14 +9,13 @@ async function getUserRepositories(username) {
         while (hasNextPage) {
             const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
                 params: {
-                    per_page: 100, // Maximum number of items per page
+                    per_page: 100, 
                     page: page
                 }
             });
-
-            repositories = repositories.concat(response.data.map(repo => repo.name));
-
-          if (response.headers.link) {
+            const reposToAdd = includeForks ? response.data : response.data.filter(repo => !repo.fork);
+            repositories = repositories.concat(reposToAdd);
+            if (response.headers.link) {
                 const linkHeader = response.headers.link;
                 hasNextPage = linkHeader.includes('rel="next"');
             } else {
@@ -24,17 +23,29 @@ async function getUserRepositories(username) {
             }
             page++;
         }
-
-        return repositories;
+        if (sortBy) {
+            repositories.sort((a, b) => {
+                if (sortBy === 'stars') {
+                    return b.stargazers_count - a.stargazers_count;
+                } else if (sortBy === 'last_updated') {
+                    return new Date(b.updated_at) - new Date(a.updated_at);
+                } else {
+                    return 0;
+                }
+            });
+        }
+        return repositories.map(repo => repo.name);
     } catch (error) {
         throw error;
     }
 }
 
-// // TEST
+//// Test
 // const username = 'zakarialaoui10';
+// const includeForks = false; 
+// const sortBy = 'stars'; 
 
-// getUserRepositories(username)
+// getUserRepositories(username, includeForks, sortBy)
 //     .then(repositories => {
 //         console.log(repositories);
 //     })
