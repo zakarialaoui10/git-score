ec9f291132bf00512ed56bf9b2e437b54ffeec0c
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-05-02 10:42:46 +0100

    migrate from cheerio to fetch-doc

--- github/getters/user/sponsoring.js
+++ github/getters/user/sponsoring.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {load} from "cheerio";
+import FetchDoc from "fetch-doc";
 
 /**
  * Fetches the list of GitHub users sponsored by the given user.
@@ -9,16 +10,19 @@ import {load} from "cheerio";
  */
 const get_sponsoring= async (login)=>{
    try {
-    const { data : html } = await axios.get(`https://github.com/${login}?tab=sponsoring`);
-    const $ = load(html);
-    const element = $("#user-profile-frame > div > div > div")
-     const arr = [];
-     element.children().each((index, child) => {
-         if (index === 0) return;
-         const sponsoring = $(child).children().eq(1).children().eq(0).children().eq(1).text().trim();
-         arr.push(sponsoring);
-     });
-     return arr;
+    // const { data : html } = await axios.get(`https://github.com/${login}?tab=sponsoring`);
+    // const $ = load(html);
+    // const element = $("#user-profile-frame > div > div > div")
+    //  const arr = [];
+    //  element.children().each((index, child) => {
+    //      if (index === 0) return;
+    //      const sponsoring = $(child).children().eq(1).children().eq(0).children().eq(1).text().trim();
+    //      arr.push(sponsoring);
+    //  });
+    //  return arr;
+    const doc= FetchDoc.sync(`https://github.com/${login}?tab=sponsoring`);
+    const sposoring=[...doc.querySelectorAll("#user-profile-frame > div > div > div > div span.Link--secondary")].map(n=>n.textContent);
+    return sposoring;
    }
    catch(error) {
       console.error('Error fetching page:', error);
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-import { Score } from "./github/score/score.js"
-import { check_repo_state } from "./github/checkers/check-repo-state-clutser.js";
-// let a=new Score("zakarialaoui10","mouadziani");
-// a.init().then(e=>console.log(e))
+// import { Score } from "./github/score/score.js"
+// import { check_repo_state } from "./github/checkers/check-repo-state-clutser.js";
+// import { weights } from "./github/score/wieghts.js"
+// // let a=new Score("zakarialaoui10","mouadziani");
+// // a.init().then(e=>console.log(e))
 
-check_repo_state("mouadziani","zakarialaoui10","ziko.js").then(e=>console.log(e))
+// //check_repo_state("mouadziani","zakarialaoui10","ziko.js").then(e=>console.log(e))
+// weights("zakaeialaoui10").then(e=>console.log(e))
+
+import { get_sponsoring } from "./github/getters/user/sponsoring.js";
+get_sponsoring("sindresorhus").then(e=>console.log(e))
